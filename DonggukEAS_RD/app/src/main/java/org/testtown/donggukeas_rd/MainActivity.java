6b6282913945b3a10c2b7bb9f170a2ba02edb1b8
한재현 <restrain22@gmail.com>
2017-11-26 14:58:44 +0900

    저장 앱 업로드

---
+++ DonggukEAS_RD/app/src/main/java/org/testtown/donggukeas_rd/MainActivity.java
@@ -0,0 +1,90 @@
+package org.testtown.donggukeas_rd;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        readText();
+
+    }
+    public void readText() {
+        database = database.getInstance();
+        reference = database.getReference();
+        AssetManager assetManager = getApplication().getAssets();
+        int stuNo=48;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(assetManager.open("DBInfo.txt")));
+            String str = null;
+
+            while ((str = br.readLine())!= null){
+
+                String[] numbers = str.split(" ");
+                stuModel sm = new stuModel(numbers[1],"-1","-1",0,"1234",numbers[0]);
+
+                Map<String, Object> newSM = new HashMap<>();
+                Map<String, Object> csAuth = sm.stu_auth_Map();
+
+                newSM.put(sm.studentId, csAuth);
+                reference.child("STUDENT_AUTH").updateChildren(newSM);
+
+                Map<String, Object> newSM1 = new HashMap<>();
+                Map<String, Object> csDev = sm.stu_dev_Map();
+                newSM1.put(sm.studentId, csDev);
+                reference.child("STUDENT_DEVICE").updateChildren(newSM1);
+
+                Map<String, Object> newSM2 = new HashMap<>();
+                Map<String, Object> csWifi = sm.stu_wifi_Map();
+                newSM2.put(sm.studentId, csWifi);
+                reference.child("STUDENT_WIFI").updateChildren(newSM2);
+
+                Map<String, Object> newSM3 = new HashMap<>();
+                Map<String, Object> csMap = sm.stu_Map();
+                newSM3.put(sm.studentId, csMap);
+                reference.child("STUDENT").updateChildren(newSM3);
+
+                Map<String, Object> newSM4 = new HashMap<>();
+                Map<String, Object> csAtd = sm.stu_atd_Map();
+                newSM4.put(sm.studentId, csAtd);
+                for(int index=1;index<=16;index++) {
+                    reference.child("STUDENT_ATTENDANCE").child("CSE4058-02").child(String.valueOf(index)).updateChildren(newSM4);
+                }
+                Map<String, Object> newSM5 = new HashMap<>();
+
+                newSM5.put(String.valueOf(stuNo), sm.studentId);
+                reference.child("SUBJECT").child("CSE4058-02").child("listenStudent").updateChildren(newSM5);
+
+
+                stuNo-=1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null)try {
+                Map<String, Object> newSM6 = new HashMap<>();
+                newSM6.put("days", "0101000");
+                reference.child("SUBJECT").child("CSE4058-02").updateChildren(newSM6);
+                newSM6.put("subjectCode", "CSE4058-02");
+                reference.child("SUBJECT").child("CSE4058-02").updateChildren(newSM6);
+                newSM6.put("subjectName", "소프트웨어공학개론");
+                reference.child("SUBJECT").child("CSE4058-02").updateChildren(newSM6);
+                br.close();
+            }catch (IOException e){}
+        }
+    }
+}
---
+++ DonggukEAS_RD/app/src/main/java/org/testtown/donggukeas_rd/stuModel.java
@@ -0,0 +1,106 @@
+package org.testtown.donggukeas_rd;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by restr on 2017-11-21.
+ */
+
+public class stuModel {
+    String studentId, deviceToken,wifiInfo,pw,name;
+    int attendanceStatus;
+    public stuModel() {
+    }
+
+    public stuModel(String studentId, String deviceToken, String wifiInfo, int attendanceStatus, String pw, String name) {
+        this.studentId = studentId;
+        this.deviceToken = deviceToken;
+        this.wifiInfo = wifiInfo;
+        this.attendanceStatus = attendanceStatus;
+        this.pw = pw;
+        this.name = name;
+    }
+
+    public Map<String,Object> stu_dev_Map(){
+        HashMap<String,Object> result = new HashMap<>();
+        result.put("studentId",studentId);
+        result.put("deviceToken",deviceToken);
+        return result;
+    }
+    public Map<String,Object>  stu_auth_Map(){
+        HashMap<String,Object> result = new HashMap<>();
+        result.put("id",studentId);
+        result.put("pw",pw);
+        return result;
+    }
+    public Map<String,Object>  stu_wifi_Map(){
+        HashMap<String,Object> result = new HashMap<>();
+        result.put("studentId",studentId);
+        result.put("wifiInfo",wifiInfo);
+        return result;
+    }
+    public Map<String,Object>  stu_Map(){
+        HashMap<String,Object> result = new HashMap<>();
+        HashMap<String,Object> result1 = new HashMap<>();
+        result1.put("0","CSE4058-02");
+        result.put("studentId",studentId);
+        result.put("studentName",name);
+        result.put("listenSubject",result1);
+        return result;
+    }
+    public Map<String,Object>  stu_atd_Map(){
+        HashMap<String,Object> result = new HashMap<>();
+        result.put("studentId",studentId);
+        result.put("attendanceStatus",attendanceStatus);
+        return result;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+
+    public String getWifiInfo() {
+        return wifiInfo;
+    }
+
+    public void setWifiInfo(String wifiInfo) {
+        this.wifiInfo = wifiInfo;
+    }
+
+    public int getAttendanceStatus() {
+        return attendanceStatus;
+    }
+
+    public void setAttendanceStatus(int attendanceStatus) {
+        this.attendanceStatus = attendanceStatus;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
