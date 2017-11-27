126dadbaed55e8122311f3c04177c71a37c08bc3
한승범 <hanseungbeom@hanseungbeom-ui-MacBook-Pro.local>
2017-11-27 17:47:08 +0900

    client login enter key

--- DonggukEAS_client/app/src/main/java/dgu/donggukeas_client/ui/LoginActivity.java
+++ DonggukEAS_client/app/src/main/java/dgu/donggukeas_client/ui/LoginActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,11 +33,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 //abc
         mIdEt = (EditText) findViewById(R.id.et_id);
         mPwEt = (EditText) findViewById(R.id.et_pw);
         mloginBtn = (Button) findViewById(R.id.btn_login);
-        Log.d("#####",FirebaseInstanceId.getInstance().getToken());
+        mPwEt.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                //Enter key Action
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    //Enter키눌렀을떄 처리
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
+       // Log.d("#####",FirebaseInstanceId.getInstance().getToken());
 
 
 /*
@@ -61,46 +76,50 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //로그인 부분 firebase 에서 해당아이디에 관한 정보를 가져와 비교한다
-                final StudentAuth inputStudentAuth = new StudentAuth(mIdEt.getText().toString(),mPwEt.getText().toString());
-                mDatabaseClients.child(inputStudentAuth.getId()).addListenerForSingleValueEvent(
-                        new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                // Get user value
-                                StudentAuth studentAuth = dataSnapshot.getValue(StudentAuth.class);
-                                if(studentAuth !=null) {
-
-                                    if (inputStudentAuth.getPw().equals(studentAuth.getPw())) {
-                                        Log.d("#####", studentAuth.getId() + "success");
-                                        Intent i = new Intent(LoginActivity.this,MainActivity.class);
-                                        i.putExtra(getString(R.string.extra_id), studentAuth.getId());
-                                        Toast.makeText(LoginActivity.this,getString(R.string.success_login),Toast.LENGTH_SHORT).show();
-                                        startActivity(i);
-
-                                    }
-
-                                    else {
-                                        Log.d("#####", studentAuth.getId() + "failed");
-                                        Toast.makeText(LoginActivity.this,getString(R.string.failed_login_pw),Toast.LENGTH_SHORT).show();
-
-                                        // ...
-                                    }
-                                }
-                                else{
-
-                                    Log.d("#####", inputStudentAuth.getId()+"cannot find");
-                                    Toast.makeText(LoginActivity.this,getString(R.string.failed_login_id),Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-                                Log.w("#####", "getUser:onCancelled", databaseError.toException());
-                            }
-                        });
+                login();
             }
         });
+
+    }
+    public void login(){
+        final StudentAuth inputStudentAuth = new StudentAuth(mIdEt.getText().toString(),mPwEt.getText().toString());
+        mDatabaseClients.child(inputStudentAuth.getId()).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        StudentAuth studentAuth = dataSnapshot.getValue(StudentAuth.class);
+                        if(studentAuth !=null) {
+
+                            if (inputStudentAuth.getPw().equals(studentAuth.getPw())) {
+                                Log.d("#####", studentAuth.getId() + "success");
+                                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                                i.putExtra(getString(R.string.extra_id), studentAuth.getId());
+                                Toast.makeText(LoginActivity.this,getString(R.string.success_login),Toast.LENGTH_SHORT).show();
+                                startActivity(i);
+
+                            }
+
+                            else {
+                                Log.d("#####", studentAuth.getId() + "failed");
+                                Toast.makeText(LoginActivity.this,getString(R.string.failed_login_pw),Toast.LENGTH_SHORT).show();
+
+                                // ...
+                            }
+                        }
+                        else{
+
+                            Log.d("#####", inputStudentAuth.getId()+"cannot find");
+                            Toast.makeText(LoginActivity.this,getString(R.string.failed_login_id),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("#####", "getUser:onCancelled", databaseError.toException());
+                    }
+                });
     }
 
 }
